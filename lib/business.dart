1de11ad45a45dc2ac3047e0dbf51ec9bb086f297
JackShih <pyopoly@gmail.com>
2021-04-29 16:36:52 -0700

    Firebase can be initialized. But not grabbing any data from firebase yet. Business page has a list of dummy json data from www.json-generator.com

--- lib/business.dart
+++ lib/business.dart
@@ -1,9 +1,126 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Business extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(20.0), child: Text('Business Page'));
+      padding: EdgeInsets.all(20.0),
+      child: new MyHomePage(),
+      // child: ListView(children: [
+      //   Container(
+      //     padding: EdgeInsets.all(20.0),
+      //     child: Text('Business Page'),
+      //   ),
+      //   Text('Business Page')
+      // ])
+    );
   }
 }
+
+// class BusinessContainer extends Container {
+//   BusinessContainer(String name, String address, String description) {
+//     this.
+//   }
+// }
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<List<User>> _getUsers() async {
+    var data = await http
+        .get("http://www.json-generator.com/api/json/get/bTNxwmMawi?indent=2");
+
+    var jsonData = json.decode(data.body);
+
+    List<User> users = [];
+
+    for (var u in jsonData) {
+      User user =
+          User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
+
+      users.add(user);
+    }
+
+    print(users.length);
+
+    return users;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: Container(
+        child: FutureBuilder(
+          future: _getUsers(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            print(snapshot.data);
+            if (snapshot.data == null) {
+              return Container(child: Center(child: Text("Loading...")));
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(snapshot.data[index].picture),
+                    ),
+                    title: Text(snapshot.data[index].name),
+                    subtitle: Text(snapshot.data[index].email),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) =>
+                                  DetailPage(snapshot.data[index])));
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final User user;
+
+  DetailPage(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: Text(user.name),
+    ));
+  }
+}
+
+class User {
+  final int index;
+  final String about;
+  final String name;
+  final String email;
+  final String picture;
+
+  User(this.index, this.about, this.name, this.email, this.picture);
+}
+
+// class Business {
+//   final int index;
+//   final String name;
+//   final String address;
+//   final String description;
+//
+//   Business(this.index, this.name, this.address, this.description);
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,56 @@ import 'landing.dart';
 import 'calendar.dart';
 import 'hike.dart';
 
-void main() {
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vanderhoof App Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Landing Page'),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return new Text(
+              "Something went wrong: ${snapshot.hasError.toString()}",
+              textDirection: TextDirection.ltr);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Vanderhoof App Flutter Demo',
+            theme: ThemeData(
+              // This is the theme of your application.
+              //
+              // Try running your application with "flutter run". You'll see the
+              // application has a blue toolbar. Then, without quitting the app, try
+              // changing the primarySwatch below to Colors.green and then invoke
+              // "hot reload" (press "r" in the console where you ran "flutter run",
+              // or simply save your changes to "hot reload" in a Flutter IDE).
+              // Notice that the counter didn't reset back to zero; the application
+              // is not restarted.
+              primarySwatch: Colors.blue,
+            ),
+            home: MyHomePage(title: 'Landing Page'),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
