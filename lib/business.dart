f87e4f23276aa0ad84df98130a3fd3322ef60fe3
Jason Cheung <jason.cheung@live.ca>
2021-04-29 21:37:14 -0700

    search functionality incomplete - check todo

--- lib/business.dart
+++ lib/business.dart
@@ -10,50 +10,79 @@ class Business extends StatefulWidget {
 }
 
 class _BusinessPageState extends State<Business> {
+  List items = [];
+  List filteredSearchItems = [];
+  bool isSearching = false;
+
+  @override
+  void initState() {
+    // reference: https://github.com/bitfumes/flutter-country-house/blob/master/lib/Screens/AllCountries.dart
+    // todo: get firebase data and populate into list of items
+
+    // getItems().then((data) {
+    //   setState(() {
+    //     items = filteredSearchItems = data;
+    //   });
+    // });
+    super.initState();
+  }
+
+  // This method does the logic for search
+  // reference: https://github.com/bitfumes/flutter-country-house/blob/master/lib/Screens/AllCountries.dart
+  // todo: replace objClass so value will match with object name
+  void _filterSearchItems(value) {
+    setState(() {
+      filteredSearchItems = items
+          .where((objClass) =>
+              objClass['name'].toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       // appBar: AppBar(
-      //   backgroundColor: Colors.pink,
-      //   title: !isSearching
-      //       ? Text('All Countries')
-      //       : TextField(
-      //           onChanged: (value) {
-      //             _filterCountries(value);
-      //           },
-      //           style: TextStyle(color: Colors.white),
-      //           decoration: InputDecoration(
-      //               icon: Icon(
-      //                 Icons.search,
-      //                 color: Colors.white,
-      //               ),
-      //               hintText: "Search Country Here",
-      //               hintStyle: TextStyle(color: Colors.white)),
-      //         ),
-      //   actions: <Widget>[
-      //     isSearching
-      //         ? IconButton(
-      //             icon: Icon(Icons.cancel),
-      //             onPressed: () {
-      //               setState(() {
-      //                 this.isSearching = false;
-      //                 filteredCountries = countries;
-      //               });
-      //             },
-      //           )
-      //         : IconButton(
-      //             icon: Icon(Icons.search),
-      //             onPressed: () {
-      //               setState(() {
-      //                 this.isSearching = true;
-      //               });
-      //             },
-      //           )
-      //   ],
+      //   title: Text(widget.title),
       // ),
+      appBar: AppBar(
+        title: !isSearching
+            ? Text(widget.title)
+            : TextField(
+                onChanged: (value) {
+                  // search logic here
+                  _filterSearchItems(value);
+                },
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    hintText: "Search Businesses",
+                    hintStyle: TextStyle(color: Colors.white70)),
+              ),
+        actions: <Widget>[
+          isSearching
+              ? IconButton(
+                  icon: Icon(Icons.cancel),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = false;
+                      filteredSearchItems = items;
+                    });
+                  },
+                )
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {
+                      this.isSearching = true;
+                    });
+                  },
+                )
+        ],
+      ),
       body: Container(
         padding: EdgeInsets.all(20.0),
         child: Column(
