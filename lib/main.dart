8a475e79278874659f8dc07ec61c951dfb85bbe6
Jason Cheung <jason.cheung@live.ca>
2021-04-29 17:11:33 -0700

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,7 @@ class MyApp extends StatelessWidget {
       title: 'Vanderhoof App Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        primarySwatch: colorPrimary,
         primaryColor: colorPrimary,
         accentColor: colorAccent,
         // canvasColor: colorBackground,
@@ -73,8 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // This method is rerun every time setState is called.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
@@ -121,6 +113,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+// This method uses a Color with a hex code to create a MaterialColor object
 MaterialColor createMaterialColor(Color color) {
   List strengths = <double>[.05];
   Map swatch = <int, Color>{};
