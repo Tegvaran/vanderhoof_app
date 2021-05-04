64009b4de09601bd6fc24e3070407457b6f689dd
JackShih <pyopoly@gmail.com>
2021-05-03 21:02:51 -0700

    addBusinessPage.dart page improved with validation and UI improvements

--- lib/addBusinessPage.dart
+++ lib/addBusinessPage.dart
@@ -45,15 +45,49 @@ class _AddBusinessPageSate extends State<AddBusinessPage> {
                                     text: 'Business information',
                                   ),
                                   SizedBox(height: 20),
-                                  _getTextField("bus_name", "Name"),
-                                  _getTextField("address", "Address"),
-                                  _getTextField("phone", "Phone"),
-                                  _getTextField("website", "Website"),
-                                  _getTextField("email", "Email"),
-                                  _getTextField("description", "Description"),
+                                  _getTextField(
+                                      "bus_name",
+                                      "Name",
+                                      "Vanderhoof Chamber of Commerce",
+                                      Icon(Icons.account_balance)),
+                                  _getTextField(
+                                      "address",
+                                      "Address",
+                                      "188 E Stewart Street, Unit 11, PO Box 126, Vanderhoof, BC,",
+                                      Icon(Icons.add_location_alt_outlined)),
+                                  _getTextField("phone", "Phone",
+                                      "604-123-1234", Icon(Icons.phone),
+                                      phone: true),
+                                  _getTextField("website", "Website",
+                                      "www.example.com", Icon(Icons.web),
+                                      url: true),
+                                  _getTextField(
+                                      "email",
+                                      "Email",
+                                      "example@gmail.com",
+                                      Icon(Icons.email_outlined),
+                                      email: true),
+                                  _getTextField(
+                                      "description",
+                                      "Description",
+                                      "description of business",
+                                      Icon(Icons.description_outlined)),
                                   SizedBox(height: 20),
                                   ElevatedButton(
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      print("-------------------------");
+
+                                      final validationSuccess =
+                                          _formKey.currentState.validate();
+                                      if (validationSuccess) {
+                                        _formKey.currentState.save();
+                                        print(_formKey.currentState.value);
+                                        _formKey.currentState.value
+                                            .forEach((key, value) {
+                                          print("${key}  and  ${value}");
+                                        });
+                                      }
+                                    },
                                     child: Text('Submit'),
                                   )
                                 ],
@@ -79,18 +113,51 @@ class _AddBusinessPageSate extends State<AddBusinessPage> {
     );
   }
 
-  Widget _getTextField(String name, String hintText) {
+  Widget _getTextField(
+      String name, String labelText, String hintText, Icon icon,
+      {email = false, url = false, phone = false}) {
+    var formValidator;
+    if (email == true) {
+      formValidator = FormBuilderValidators.compose([
+        FormBuilderValidators.required(context),
+        FormBuilderValidators.email(context)
+      ]);
+    } else if (url == true) {
+      formValidator = FormBuilderValidators.compose([
+        FormBuilderValidators.required(context),
+        FormBuilderValidators.url(context)
+      ]);
+    } else if (phone == true) {
+      formValidator = FormBuilderValidators.compose([
+        FormBuilderValidators.required(context),
+        (value) {
+          value = value.replaceAll('-', '');
+          if (!RegExp(r'^[0-9]+$').hasMatch(value) || value.length != 10) {
+            return "This field must be a phone number";
+          } else {
+            return null;
+          }
+        }
+      ]);
+    } else {
+      formValidator = FormBuilderValidators.required(context);
+    }
     return Container(
       margin: EdgeInsets.only(top: 15),
       child: FormBuilderTextField(
         name: name,
+        validator: formValidator,
+        // onTap: () => {},
         decoration: InputDecoration(
+          labelText: labelText,
+          hintText: hintText,
+          icon: icon,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
           border: OutlineInputBorder(
             borderRadius: const BorderRadius.all(
               const Radius.circular(10.0),
             ),
           ),
-          hintText: hintText,
         ),
       ),
     );
