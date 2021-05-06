51b2ab02b345cab4d8f11eb6d95f813304abd0ee
Jason Cheung <jason.cheung@live.ca>
2021-05-06 13:17:18 -0700

    comments

--- lib/commonFunction.dart
+++ lib/commonFunction.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:geocoder/geocoder.dart';
 
+/// uses an address String and returns a LatLng geopoint
 Future<GeoPoint> toLatLng(String addr) async {
   var address = await Geocoder.local.findAddressesFromQuery(addr);
   var first = address.first;
@@ -8,4 +10,25 @@ Future<GeoPoint> toLatLng(String addr) async {
   var lat = coor.latitude;
   var lng = coor.longitude;
   return GeoPoint(lat, lng);
+}
+
+/// uses a Color with a hex code and returns a MaterialColor object
+MaterialColor createMaterialColor(Color color) {
+  List strengths = <double>[.05];
+  Map swatch = <int, Color>{};
+  final int r = color.red, g = color.green, b = color.blue;
+
+  for (int i = 1; i < 10; i++) {
+    strengths.add(0.1 * i);
+  }
+  strengths.forEach((strength) {
+    final double ds = 0.5 - strength;
+    swatch[(strength * 1000).round()] = Color.fromRGBO(
+      r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+      g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+      b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+      1,
+    );
+  });
+  return MaterialColor(color.value, swatch);
 }
