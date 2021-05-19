dfd70a474f2d4146e4e065123611979d4e2749b7
Xaitin <xaitin120@gmail.com>
2021-05-18 21:46:17 -0700

    Small bugfix

--- lib/resource.dart
+++ lib/resource.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
