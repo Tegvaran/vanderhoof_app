e5eed04f2573cfc00a78050ac9cb20e9681daf30
Jason Cheung <jason.cheung@live.ca>
2021-05-21 00:45:23 -0700

    changed TextButton to ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -142,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         padding: const EdgeInsets.all(12.0),
         child: Align(
           alignment: Alignment.center,
-          child: TextButton.icon(
+          child: ElevatedButton.icon(
               style: TextButton.styleFrom(
                   backgroundColor: colorPrimary,
                   primary: colorAccent,
